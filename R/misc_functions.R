d52cb81e51edcb29e3b8cbaff0c417102d84866c
Temi <tadepete@gmail.com>
2022-01-26 09:04:10 -0600

    First commit Created misc_functions.R script

---
+++ R/misc_functions.R
@@ -0,0 +1,44 @@
+
+
+# ===============
+# Usage: Miscellaneous R functions I use
+# Author: Temi
+# 
+# 
+#
+
+#' A genetic qqplot
+#' @param 
+#' @param
+#' @return a qqplot object
+qq_generic <- function(data_points, distribution=c('normal', 'chisq', 'uniform', 't'), neg_log10_values=F, mean=0, sd=1, df=1, min=0, max=1){
+    
+    n <- length(data_points)
+    if(length(distribution) > 1){
+        stop('Input one distribution.')
+    }
+    
+    theoretical_quantiles <- switch(as.character(distribution), 
+                                    'normal' = qnorm(ppoints(n), mean = mean, sd = sd)[order(order(data_points))],
+                                    'chisq' = qchisq(ppoints(n), df=df)[order(order(data_points))],
+                                    'uniform' = qunif(ppoints(n), min=min, max=max)[order(order(data_points))],
+                                    't' = qt(ppoints(n), df=df)[order(order(data_points))],
+                                    stop('The distribution is invalid. Input a valid distribution.')
+    )
+    
+    xlab_use <- 'Theoretical Quantiles'
+    ylab_use <- 'Sample Quantiles'
+    
+    if(neg_log10_values == T){
+        theoretical_quantiles <- -log10(theoretical_quantiles)
+        data_points <- -log10(data_points)
+        xlab_use <- '-log10(Theoretical quantiles)'
+        ylab_use <- '-log10(Sample quantiles)'
+    }
+    
+    plot(theoretical_quantiles, data_points, xlab = xlab_use, ylab = ylab_use)
+    abline(a = 0, b = 1, col = "red")
+    
+}
+
+#qq_generic(result$observed_chistats, distribution = 'normal')
